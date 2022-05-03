237cada21e5ee2f5d04fbf0175a59e56a048e1d6
fred <abnertorres58@gmail.com>
2022-05-02 20:24:35 -0400

    Spring REST - REST Controller Demo - Creating the REST Controller

---
+++ src/main/java/com/freudromero/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package com.freudromero.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.freudromero.springdemo")
+public class DemoAppConfig {
+}

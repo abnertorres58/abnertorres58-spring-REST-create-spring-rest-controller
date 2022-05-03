b42c42db4b6c3986e2e5dec743509632fed4243c
fred <abnertorres58@gmail.com>
2022-05-03 00:07:47 -0400

    Spring REST - Exception Handling - Adding Generic Exception Handler

--- src/main/java/com/freudromero/springdemo/rest/StudentRestController.java
+++ src/main/java/com/freudromero/springdemo/rest/StudentRestController.java
@@ -64,4 +64,20 @@ public class StudentRestController {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    // Add another exception handler ... to catch any exception (catch all)
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // Return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+
 }
